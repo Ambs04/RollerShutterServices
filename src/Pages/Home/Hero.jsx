85b5c02c1705d03391c4530707c4f6ca487fa013
Amber Jones <amberleejones2007@gmail.com>
2026-08-07 23:49:41 +0200

    hero styling complete

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -2,18 +2,25 @@ import hero from "../../assets/hero.png";
 
 export default function Hero() {
   return (
-    <>
-      <div>
-        <div>
-          <img src={hero} />
-          <div />
-          <h2>Welcome To RSS</h2>
-          <p>Cape Town's Leaders in Roller Shutter Doors</p>
+    <section class=" overflow-hidden">
+      <img src={hero} class="absolute inset-0 w-full h-3/4 object-cover" />
+      <div class="absolute h-3/4 inset-0 bg-black/60">
+        <div />
+        <div class="relative top-20 z-10 max-w-7xl mx-auto px-6 sm:px-4 lg:px-8 py-40 min-h-screen flex flex-col items-center ">
+          <h2 class="text-4xl font-semibold tracking-wider text-[#e0e1e6] sm:text-6xl text-center">
+            Welcome To <span class="text-[#e0e1e6]">R</span>
+            <span class="text-[#e0e1e6]">SS</span>
+          </h2>
+          <p class="py-6 text-[#e0e1e6] text-sm sm:text-lg text-center">
+            Cape Town's Leaders in Roller Shutter Doors
+          </p>
           <i>
-            <p>"Where we keep you, rolling."</p>
+            <p class="text-[#f22233] font-medium">
+              "Where we keep you, <span class="text-[#f2b90f]">rolling</span>."
+            </p>
           </i>
         </div>
       </div>
-    </>
+    </section>
   );
 }
