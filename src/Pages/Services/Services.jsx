85c9a86c131481aac389f5e40a82e00707199a62
Amber Jones <amberleejones2007@gmail.com>
2026-08-11 12:27:38 +0200

    services animation complete

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -52,7 +52,9 @@ export default function Services() {
       <Header />
       <div class="py-30 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="mt-12 mb-16 text-center">
-          <h1 class="text-[#e0e1e6] text-xl underline mb-3">Our Services</h1>
+          <h1 class="text-[#e0e1e6] text-xl underline mb-3" data-aos="fade-in">
+            Our Services
+          </h1>
           <p class="text-[#e0e1e6]/50">
             We offer a range of services from installation of roller shutters to
             the installation of window shutters.
@@ -63,6 +65,7 @@ export default function Services() {
             <div
               key={index}
               class="group overflow-hidden rounded-2xl border border-white/10 bg-[#181818] transition-all duration-300 hover:-translate-y-2 hover:border-[#F22233]/40 hover:bg-[#202020]"
+              data-aos="fade-right"
             >
               <div class="h-2/4 w-full">
                 <img
@@ -70,7 +73,7 @@ export default function Services() {
                   class="object-cover h-full w-full shadow-2xl"
                 />
               </div>
-              <div class="text-center mt-6">
+              <div class="text-center mt-6  flex flex-col justify-center items-center lg:justify-start ">
                 <p class="mb-3 text-[#e0e1e6]/70 text-lg px-3">{i.title}</p>
                 <p class="text-[#e0e1e6]/40 text-sm px-3">{i.description}</p>
               </div>
